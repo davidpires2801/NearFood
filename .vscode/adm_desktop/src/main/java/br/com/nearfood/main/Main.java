b5172bf5733b2a64e32c2ee6796f975eccc51e9e
davidpires2801 <davidesplossyesplossy05@gmail.com>
2025-08-29 08:05:14 -0300

    Create Main.java

---
+++ .vscode/adm_desktop/src/main/java/br/com/nearfood/main/Main.java
@@ -0,0 +1,14 @@
+
+package br.com.nearfood.main;
+
+/**
+ *
+ * @author firmodev
+ */
+public class Main {
+    public static void main(String []args ){
+        System.out.println("teste de print");
+    }
+        
+    
+}
